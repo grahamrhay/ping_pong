c7fac4de727329c035cd48a4c6d8e3d5d71f1511
Graham Hay <grahamrhay@gmail.com>
2014-12-21 18:39:11 +0000

    Handle json

--- src/ws_handler.erl
+++ src/ws_handler.erl
@@ -7,9 +7,11 @@
 init(Req, Opts) ->
     {cowboy_websocket, Req, Opts}.
 
-websocket_handle({text, <<"ping">>}, Req, State) ->
-    Reply = {text, <<"pong">>},
-    {reply, Reply, Req, State};
+websocket_handle({text, Json}, Req, State) ->
+    Map = jiffy:decode(Json, [return_maps]),
+    Count = maps:get(<<"count">>, Map),
+    Reply = #{type => <<"pong">>, count => Count + 1},
+    {reply, {text, jiffy:encode(Reply)}, Req, State};
 
 websocket_handle(Frame, Req, State) ->
     io:format("Unexpected frame: ~p~n", [Frame]),
